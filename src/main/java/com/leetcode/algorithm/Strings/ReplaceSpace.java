fb7a274be8b54fa38edf5dc14ff81e992377093a
lskyline <lclskyline@gmail.com>
2021-04-09 06:53:41 +0800

    替换空格第二种方式

--- src/main/java/com/leetcode/algorithm/Strings/ReplaceSpace.java
+++ src/main/java/com/leetcode/algorithm/Strings/ReplaceSpace.java
@@ -32,11 +32,36 @@ public class ReplaceSpace {
         return sb.toString();
     }
 
+    public String replaceSpace02(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        int p1 = sb.length() - 1;
+        for (int i = 0; i <= p1; i++) {
+            if (sb.charAt(i) == ' ') {
+                sb.append("  ");
+            }
+        }
+
+        int p2 = sb.length() - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            char c = sb.charAt(p1--);
+            if (c== ' ') {
+                sb.setCharAt(p2--, '0');
+                sb.setCharAt(p2--, '2');
+                sb.setCharAt(p2--, '%');
+            } else {
+                sb.setCharAt(p2--, c);
+            }
+        }
+        return sb.toString();
+    }
+
 
     public static void main(String[] args) {
         ReplaceSpace obj = new ReplaceSpace();
         String str = "We are happy";
-        String replaceStr = obj.replaceSpace(str);
+        String replaceStr = obj.replaceSpace02(str);
         System.out.println(replaceStr);
+
+        StringBuilder sb = new StringBuilder(str);
     }
 }
