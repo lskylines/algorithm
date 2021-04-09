d897c692623ab9e6cd7f0f384499eb2568970622
lskyline <lclskyline@gmail.com>
2021-04-09 09:22:04 +0800

    跳台阶

---
+++ src/main/java/com/leetcode/algorithm/num/JumpFloor.java
@@ -0,0 +1,33 @@
+package com.leetcode.algorithm.num;
+
+/**
+ * @ ClassName JumpFloor
+ * @ author lskyline
+ * @ 2021/4/9 9:19
+ * @ Version: 1.0
+ */
+public class JumpFloor {
+    /**
+     * 跳台阶
+     */
+    public int jumpFloor(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int pre1 = 1;
+        int pre2 = 2;
+        int result = 0;
+        for (int i = 2; i <= n; i++) {
+            result = pre1 + pre2;
+            pre1 = pre2;
+            pre2 = result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        JumpFloor obj = new JumpFloor();
+        int result = obj.jumpFloor(3);
+        System.out.println(result);
+    }
+}
