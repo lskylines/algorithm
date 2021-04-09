bc10b60fa1d22ffdb36bef61be370fc66ee17902
lskyline <lclskyline@gmail.com>
2021-04-09 09:40:45 +0800

    变态跳台阶

---
+++ src/main/java/com/leetcode/algorithm/num/JumpFloorII.java
@@ -0,0 +1,20 @@
+package com.leetcode.algorithm.num;
+
+/**
+ * @ ClassName jumpFloorII
+ * @ author lskyline
+ * @ 2021/4/9 9:27
+ * @ Version: 1.0
+ */
+public class JumpFloorII {
+    /**
+     * 变态跳台阶
+     */
+    public int jumpFloorII(int n) {
+        return (int)(Math.pow(2, n -1));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
