da888b75c51897ba81b437bfc9c48b9493ee13ce
lskyline <lclskyline@gmail.com>
2021-04-21 17:02:16 +0800

    两两交换链表中的节点使用栈 + 哨兵

--- src/main/java/com/leetcode/algorithm/list/SwapPairs.java
+++ src/main/java/com/leetcode/algorithm/list/SwapPairs.java
@@ -2,6 +2,8 @@ package com.leetcode.algorithm.list;
 
 import com.leetcode.algorithm.list.Node.ListNode;
 
+import java.util.Stack;
+
 /**
  * @author lskyline
  * @ 2021/1/7 23:47
@@ -36,5 +38,44 @@ public class SwapPairs {
             pre = l1;
         }
         return node.next;
+    }
+
+    public ListNode swapPair02(ListNode head) {
+        if (head == null || head.next ==null) {
+            return head;
+        }
+        Stack<ListNode> stack = new Stack<>();
+        ListNode dummyNode = new ListNode(-1);
+        ListNode target = dummyNode;
+        dummyNode.next = head;
+        ListNode curNode = head;
+        ListNode nextNode = null;
+        while (curNode != null && curNode.next != null) {
+            stack.push(curNode);
+            stack.push(curNode.next);
+            curNode = curNode.next.next;
+            while (!stack.isEmpty()) {
+                nextNode = stack.pop();
+                nextNode.next = dummyNode.next;
+                dummyNode.next = nextNode;
+                dummyNode = dummyNode.next;
+            }
+        }
+        dummyNode.next = curNode;
+        return target.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(3);
+        root.next.next.next = new ListNode(4);
+        root.next.next.next.next = new ListNode(5);
+        SwapPairs obj = new SwapPairs();
+        ListNode dummyNode = obj.swapPair02(root);
+        while (dummyNode != null) {
+            System.out.println(dummyNode.val);
+            dummyNode = dummyNode.next;
+        }
     }
 }
