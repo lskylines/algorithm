e7942b2dbd91621ac670247364f9fd71b17fc8d3
lskyline <lclskyline@gmail.com>
2021-04-21 15:54:56 +0800

    回文链表优化

--- src/main/java/com/leetcode/algorithm/list/Palindrome02.java
+++ src/main/java/com/leetcode/algorithm/list/Palindrome02.java
@@ -9,9 +9,28 @@ import com.leetcode.algorithm.list.Node.ListNode;
  * @ Version: 1.0
  */
 public class Palindrome02 {
-    public static boolean isPalindrome(ListNode head) {
+
+    public static class Pair <T, U>{
+        private T t;
+        private U u;
+
+        public Pair(T t, U u) {
+            this.t = t;
+            this.u = u;
+        }
+
+        public T getLeft() {
+            return t;
+        }
+
+        public U getRight() {
+            return u;
+        }
+    }
+
+    public  Pair<ListNode, Boolean> isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
-            return true;
+            return new Pair<ListNode, Boolean>(null, true);
         }
         ListNode slow = head;
         ListNode fast = head.next;
@@ -22,22 +41,23 @@ public class Palindrome02 {
         ListNode firstNoe = head;
 
         ListNode tempNode = lastNode;
-
+        boolean flag = true;
         while (firstNoe != null) {
             if (firstNoe.val != lastNode.val) {
-                return false;
+                flag  = false;
+                break ;
             }
             firstNoe = firstNoe.next;
             lastNode = lastNode.next;
         }
 
         //将链表恢复原来状态
-        recoverList(tempNode);
-        return true;
+        reverseList(tempNode);
+        return new Pair<ListNode, Boolean>(head, flag);
     }
 
     //对反转的链表进行恢复操作
-    public static void recoverList(ListNode tempNode) {
+    public  void recoverList(ListNode tempNode) {
         ListNode pre = null;
         ListNode cur = tempNode;
         ListNode next = null;
@@ -50,7 +70,7 @@ public class Palindrome02 {
     }
 
     //对中点后的部分链表进行反转
-    public static ListNode reverseList(ListNode middleNode) {
+    public  ListNode reverseList(ListNode middleNode) {
         ListNode preNode = null;
         ListNode nextNode = null;
         ListNode currentNode = middleNode;
@@ -70,5 +90,22 @@ public class Palindrome02 {
             slowNode = slowNode.next;
         }
         return slowNode;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode root = new ListNode(1);
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(2);
+        root.next.next.next = new ListNode(1);
+        Palindrome02 obj = new Palindrome02();
+        Pair<ListNode, Boolean> pair = obj.isPalindrome(root);
+        System.out.println(pair.getRight());
+
+        ListNode newNode = pair.getLeft();
+        while (newNode != null) {
+            System.out.println(newNode.val);
+            newNode = newNode.next;
+        }
     }
 }
