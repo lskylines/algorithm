5fba46cd30110b1fce34cacca680077094bdb5a4
lskyline <lclskyline@gmail.com>
2021-05-02 17:28:25 +0800

    两数之和

--- src/main/java/com/leetcode/algorithm/num/TwoSum.java
+++ src/main/java/com/leetcode/algorithm/num/TwoSum.java
@@ -1,6 +1,8 @@
 package com.leetcode.algorithm.num;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @ ClassName TwoSum
@@ -11,6 +13,8 @@ import java.util.Arrays;
 public class TwoSum {
     /*
      * 两数之和
+     * 1) 暴力法
+     * 2） 哈希表
      */
     public int[] twoSum(int[] nums, int target) {
         if (nums == null || nums.length < 2) {
@@ -26,6 +30,21 @@ public class TwoSum {
             }
         }
         return res;
+    }
+
+    public int[] twoSum02(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return null;
+        }
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i , map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
