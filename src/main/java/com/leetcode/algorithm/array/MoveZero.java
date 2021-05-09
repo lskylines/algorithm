dddc60fbe63eea9d358489733bd9e1bf960f06de
lskyline <lclskyline@gmail.com>
2021-05-09 12:01:58 +0800

    移动零

--- src/main/java/com/leetcode/algorithm/array/MoveZero.java
+++ src/main/java/com/leetcode/algorithm/array/MoveZero.java
@@ -29,4 +29,18 @@ public class MoveZero {
             nums[idx++] = 0;
         }
     }
+
+    public void moveZeroes02(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return ;
+        }
+        int j = 0;
+        for (int i =0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
+            }
+        }
+    }
 }
