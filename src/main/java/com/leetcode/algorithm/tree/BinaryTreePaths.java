2ba961c4198740b623fdb65bac4af9e04d618d6f
lskyline <lclskyline@gmail.com>
2021-05-20 16:25:03 +0800

    二叉树的所有路径， 广度优先遍历实现

--- src/main/java/com/leetcode/algorithm/tree/BinaryTreePaths.java
+++ src/main/java/com/leetcode/algorithm/tree/BinaryTreePaths.java
@@ -3,7 +3,9 @@ package com.leetcode.algorithm.tree;
 import com.leetcode.algorithm.tree.Node.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @ ClassName BinaryTreePaths
@@ -14,6 +16,8 @@ import java.util.List;
 public class BinaryTreePaths {
     /**
      * 二叉树的所有路径
+     * 1) 深度优先遍历
+     * 2） 广度优先遍历
      */
     private List<String> ret = new ArrayList<>();
     public List<String> binaryTreePaths(TreeNode root) {
@@ -22,6 +26,34 @@ public class BinaryTreePaths {
         }
         backTrace(root, "");
         return ret;
+    }
+
+    public List<String> binaryTreePaths02(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<String> pathQueue = new LinkedList<>();
+        nodeQueue.offer(root);
+        pathQueue.offer(Integer.toString(root.val));
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            String path = pathQueue.poll();
+            if (node.left == null && node.right == null) {
+                paths.add(path);
+            } else {
+                if (node.left != null) {
+                    nodeQueue.offer(node.left);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.left.val).toString());
+                }
+                if (node.right != null) {
+                    nodeQueue.offer(node.right);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.right.val).toString());
+                }
+            }
+        }
+        return paths;
     }
 
     private void backTrace(TreeNode root, String s) {
