6b907229a305464c119069bed1b7c77600a49b83
lskyline <lclskyline@gmail.com>
2021-05-21 23:29:13 +0800

    二叉树的最近公共祖先-非递归

--- src/main/java/com/leetcode/algorithm/tree/LowestCommonAncestorprove.java
+++ src/main/java/com/leetcode/algorithm/tree/LowestCommonAncestorprove.java
@@ -2,6 +2,8 @@ package com.leetcode.algorithm.tree;
 
 import com.leetcode.algorithm.tree.Node.TreeNode;
 
+import java.util.*;
+
 /**
  * @ ClassName LowestCommonAncestorprove
  * @ author lskyline
@@ -11,6 +13,8 @@ import com.leetcode.algorithm.tree.Node.TreeNode;
 public class LowestCommonAncestorprove {
     /*
      * 二叉树的最近公共祖先
+     * 1) 递归
+     * 2）非递归
      */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
@@ -27,6 +31,36 @@ public class LowestCommonAncestorprove {
         return root;
     }
 
+    public TreeNode lowestCommonAncestor02(TreeNode root, TreeNode p, TreeNode q) {
+        //记录遍历到的每个节点的父节点
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        //根节点没有父节点
+        parent.put(root, null);
+        queue.add(root);
+        while (!parent.containsKey(p) || !parent.containsKey(q)) {
+            TreeNode tempNode = queue.poll();
+            if (tempNode.left != null) {
+                parent.put(tempNode.left, tempNode);
+                queue.add((tempNode.left));
+            }
+            if (tempNode.right != null) {
+                parent.put(tempNode.right, tempNode);
+                queue.offer(tempNode.right);
+            }
+        }
+
+        Set<TreeNode> ancestors = new HashSet<>();
+        while (p != null) {
+            ancestors.add(p);
+            p = parent.get(p);
+        }
+        while (!ancestors.contains(q)) {
+            q = parent.get(q);
+        }
+        return q;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(5);
@@ -39,7 +73,7 @@ public class LowestCommonAncestorprove {
         root.left.right.right = new TreeNode(4);
 
         LowestCommonAncestorprove obj = new LowestCommonAncestorprove();
-        TreeNode res = obj.lowestCommonAncestor(root, root.left, root.right);
+        TreeNode res = obj.lowestCommonAncestor02(root, root.left, root.right);
         System.out.println(res.val);
 
     }
