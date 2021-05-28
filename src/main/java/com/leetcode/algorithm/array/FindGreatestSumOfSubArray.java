514f8474c5d87119c26e34a17fc3d59fea0fb35a
lskyline <lclskyline@gmail.com>
2021-05-28 23:44:49 +0800

    连续子数组的最大和

--- src/main/java/com/leetcode/algorithm/array/FindGreatestSumOfSubArray.java
+++ src/main/java/com/leetcode/algorithm/array/FindGreatestSumOfSubArray.java
@@ -9,6 +9,7 @@ package com.leetcode.algorithm.array;
 public class FindGreatestSumOfSubArray {
     /*
      * 连续子数组的最大和
+     * 1) 动态规划
      */
     public int findGreatestSumOfSubArray(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -25,6 +26,20 @@ public class FindGreatestSumOfSubArray {
             max = Math.max(max, cur);
         }
         return max;
+    }
+
+    public int findGreatestSumOfSubArray02(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = dp[i -1] >= 0 ? nums[i] + dp[i -1] : nums[i];
+            sum = Math.max(sum, dp[i]);
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
