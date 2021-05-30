e3f8a744007c3fc115d3eb9031386b2c274d07e6
lskyline <lclskyline@gmail.com>
2021-05-30 14:58:52 +0800

    把数组排成最小的数

--- src/main/java/com/leetcode/algorithm/array/PrintMinNumber.java
+++ src/main/java/com/leetcode/algorithm/array/PrintMinNumber.java
@@ -25,10 +25,50 @@ public class PrintMinNumber {
         System.out.println(Arrays.toString(nums));
         return "";
     }
+
+
+    public String printMinNumber02(int[] num) {
+        String[] strs = new String[num.length];
+        for (int i = 0; i < num.length; i++) {
+            strs[i] = String.valueOf(num[i]);
+        }
+        quickSort(strs, 0, strs.length -1);
+        StringBuilder sb  = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s).append("-> ");
+        }
+        return sb.toString();
+    }
+
+    private void quickSort(String[] str, int low, int high) {
+        if (low >= high) {
+            return ;
+        }
+        int i = low;
+        int j = high;
+        String tmp = str[i];
+        while (i < j) {
+            while ((str[j] + str[low]).compareTo(str[low] + str[j]) >= 0 && i < j) {
+                j--;
+            }
+            while ((str[i] + str[low]).compareTo(str[low] + str[i]) <= 0 && i < j) {
+                i++;
+            }
+            tmp = str[i];
+            str[i] = str[j];
+            str[j] = tmp;
+        }
+        str[i] = str[low];
+        str[low] = tmp;
+        quickSort(str, low, i -1);
+        quickSort(str, i + 1, high);
+    }
+
+
     public static void main(String[] args) {
         int[] arr = new int[]{3, 32, 321};
         PrintMinNumber obj = new PrintMinNumber();
-        String s = obj.printMinNumber(arr);
-
+        String s = obj.printMinNumber02(arr);
+        System.out.println(s);
     }
 }
