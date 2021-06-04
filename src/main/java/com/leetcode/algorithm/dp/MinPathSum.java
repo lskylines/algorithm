ec963e3859e79d3e8ccf14e6cdb6d1fd48daab96
lskyline <lclskyline@gmail.com>
2021-06-04 15:54:32 +0800

    矩阵的最小路径和

--- src/main/java/com/leetcode/algorithm/dp/MinPathSum.java
+++ src/main/java/com/leetcode/algorithm/dp/MinPathSum.java
@@ -1,5 +1,6 @@
 package com.leetcode.algorithm.dp;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,35 +13,63 @@ import java.util.Map;
 public class MinPathSum {
     /**
      * 矩阵的最小路径和
-     * 1) 使用空间的DP
-     * 2) 不使用空间的DP
-     * 3） 递归解法
+     * 1) 二维数组的动态规划
+     * 2） 一维数组的动态规划
+     * 3） 原数组上进行的DP
+     * 4） 递归 + 记忆化
      */
 
     private Map<String, Integer> map = new HashMap<>();
+
     public int minPathSum(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0] == null ||  matrix[0].length == 0) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int[][] dp = new int[row][col];
-        dp[0][0] = matrix[0][0];
-        for (int i = 1; i < row; i++) {
-            dp[i][0] += dp[i-1][0] + matrix[i][0];
-        }
-        for (int j = 1; j < col; j++) {
-            dp[0][j] = dp[0][j-1] + matrix[0][j];
-        }
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
-                dp[i][j] = Math.min(matrix[i][j] + dp[i-1][j], matrix[i][j] + dp[i][j-1]);
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        for (int i =0;i < rows; i++) {
+            for (int j =0; j < cols; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = matrix[0][0];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j -1] + matrix[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i-1][j] + matrix[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + matrix[i][j];
+                }
             }
         }
-        return dp[row-1][col-1];
+        printMatrix(dp);
+        return dp[rows -1][cols -1];
     }
 
     public int minPathSum02(int[][] matrix) {
+        if (matrix == null || matrix[0] == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = matrix[i][j];
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] + matrix[i][j];
+                } else if (j == 0) {
+                    dp[j] = dp[j] + matrix[i][j];
+                } else {
+                    dp[j] = Math.min(dp[j], dp[j - 1]) + matrix[i][j];
+                }
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+        return dp[n - 1];
+    }
+
+    public int minPathSum03(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0] == null ||  matrix[0].length == 0) {
             return 0;
         }
@@ -65,32 +94,6 @@ public class MinPathSum {
 
 
     public int minPathSum04(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return 0;
-        }
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[][] dp = new int[rows][cols];
-        for (int i =0;i < rows; i++) {
-            for (int j =0; j < cols; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = matrix[0][0];
-                } else if (i == 0) {
-                    dp[i][j] = dp[i][j -1] + matrix[i][j];
-                } else if (j == 0) {
-                    dp[i][j] = dp[i-1][j] + matrix[i][j];
-                } else {
-                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + matrix[i][j];
-                }
-            }
-        }
-        printMatrix(dp);
-        return dp[rows -1][cols -1];
-    }
-
-
-
-    public int minPathSum03(int[][] matrix) {
         if (matrix == null || matrix.length == 0) {
             return 0;
         }
@@ -129,8 +132,8 @@ public class MinPathSum {
 
     public static void main(String[] args) {
         MinPathSum obj = new MinPathSum();
-        int[][] matrix = new int[][]{{1, 3, 5, 9}, {8, 1, 3, 4}, {5, 0, 6, 1}, {8, 8, 4, 0}};
-        int res = obj.minPathSum04(matrix);
+        int[][] matrix = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        int res = obj.minPathSum02(matrix);
         System.out.println(res);
     }
 }
