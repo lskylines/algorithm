3bfb6e06d22c89ad3f03313f2684646a266502ff
lskyline <lclskyline@gmail.com>
2021-06-05 15:13:40 +0800

    实现Trie

---
+++ src/main/java/com/leetcode/algorithm/tree/Trie.java
@@ -0,0 +1,54 @@
+package com.leetcode.algorithm.tree;
+
+/**
+ * @ ClassName Trie
+ * @ author lskyline
+ * @ 2021/6/5 15:01
+ * @ Version: 1.0
+ */
+public class Trie {
+    /*
+     * 实现 Trie (前缀树)
+     */
+    private Trie[] children;
+    private boolean isEnd;
+    public Trie() {
+        this.children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    private boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
