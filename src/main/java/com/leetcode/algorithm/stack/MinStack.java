711068cfabdadfb3860c9b131f0c5f53e6a83956
lskyline <lskyline@gmail.com>
2022-07-18 01:08:37 +0800

    getMin栈

--- src/main/java/com/leetcode/algorithm/stack/MinStack.java
+++ src/main/java/com/leetcode/algorithm/stack/MinStack.java
@@ -1,5 +1,6 @@
 package com.leetcode.algorithm.stack;
 
+import java.util.Objects;
 import java.util.Stack;
 
 /**
@@ -15,35 +16,45 @@ public class MinStack {
 
 
     /**
-     * 解题思路: 采用一个最小栈保存比较出来的最小值，可以确保每次从最小栈中取出的是最小的数
+     * 解题思路:
+     * 1. 采用一个最小栈保存比较出来的最小值
+     *  压入数据new_number时，如果最小栈为空，直接压入new_number, 如果最小栈不为空，则判断最小栈顶部数据与new_number谁更小，进行压入
      */
-    private Stack<Integer> stack;
-    private Stack<Integer> minStack;
-    private Integer minValue;
+    private Stack<Integer> STACK;
+    private Stack<Integer> MIN_STACK;
+    private Integer MIN_VALUE;
 
     public MinStack() {
-        stack = new Stack<>();
-        minStack = new Stack<>();
-        minValue = Integer.MAX_VALUE;
+        STACK = new Stack<>();
+        MIN_STACK = new Stack<>();
+        MIN_VALUE = Integer.MAX_VALUE;
     }
 
     public void push(int x) {
-        stack.push(x);
-        minValue = Math.min(minValue, x);
-        minStack.push(minValue);
+        STACK.push(x);
+        MIN_VALUE = Math.min(MIN_VALUE, x);
+        MIN_STACK.push(MIN_VALUE);
     }
 
-    public void pop() {
-        stack.pop();
-        minStack.pop();
-        minValue = minStack.isEmpty() ? Integer.MAX_VALUE : minStack.peek();
+    public void pop() throws IllegalAccessException {
+        if (STACK.isEmpty()) {
+            throw new IllegalAccessException("stack is empty");
+        }
+        STACK.pop();
+        MIN_STACK.pop();
+        //等最小栈顶元素弹出后，再将最小栈中栈顶元素取出，对最小值MIN_VALUE进行维护
+        MIN_VALUE = MIN_STACK.isEmpty() ? Integer.MAX_VALUE : MIN_STACK.peek();
+
     }
 
-    public int top() {
-        return stack.peek();
+    public int top() throws IllegalAccessException {
+        if (MIN_STACK.isEmpty()) {
+            throw new IllegalAccessException("min stack is empty");
+        }
+        return STACK.peek();
     }
 
     public int getMin() {
-        return minStack.peek();
+        return MIN_STACK.peek();
     }
 }
